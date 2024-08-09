70b834f7db267b3c61fcf66e86697c6b5e6f6992
Ana Seiculescu <anaseiculescu@gmail.com>
2024-08-09 16:02:19 +0300

    add database client

--- backend/api/src/app.controller.ts
+++ backend/api/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/artworks')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/randomnumber')
+  getRandom(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/all')
+  getAllWork() {
+    return this.appService.getAllArtwork();
   }
 }
--- backend/api/src/app.service.ts
+++ backend/api/src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import knex from 'knex';
+import { configDotenv } from 'dotenv';
 
+configDotenv();
 @Injectable()
 export class AppService {
+  dbClient: knex.Knex;
+
+  constructor() {
+    // console.log('db name', process.env.POSTGRES_DB);
+    this.dbClient = knex({
+      client: 'pg',
+      connection: {
+        host: 'localhost',
+        port: 3033,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+      },
+    });
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    const rezultat = Math.random() * 20 + 15;
+    return 'rezultatul este ' + rezultat;
+  }
+
+  async getAllArtwork() {
+    const allWork = await this.dbClient.raw(`select * from artistwork`);
+    console.log('toata munca: ', allWork.rows);
+    return allWork.rows;
   }
 }
