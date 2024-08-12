cfd367d014d866dc59a526b034115d7e38746ba7
Ana Seiculescu <anaseiculescu@gmail.com>
2024-08-12 10:10:39 +0300

    initializing a database client using Knex and setting up a connection to a PostgreSQL database

---
+++ backend/database/dbsetup.js
@@ -0,0 +1,41 @@
+import knex from "knex";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+const dbClient = knex({
+    client: "pg",
+    connection: {
+        host: "localhost",
+        port: 3033,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+    },
+});
+
+async function run() {
+    await dbClient.raw(
+        `create table artistwork (
+            id serial primary key,
+            title varchar not null,
+            description varchar,
+            image varchar,
+            clientlink varchar,
+            is_visible boolean not null
+        );
+
+        insert into artistwork (
+            title, description, image, clientlink, is_visible
+        ) values (
+            'twilight', 'nice view on the horizon', '/images/twilight.jpeg', 'https://www.google.com', true
+        ),
+        (
+	        'night', 'falling stars on the sky', '/images/night.jpeg', 'https://www.google.com', false
+        ),
+        (
+	        'vacation', 'time is in our favor this time', '/images/vacation.jpeg', 'https://www.google.com', true
+        );`
+    );
+}
+
+run();
