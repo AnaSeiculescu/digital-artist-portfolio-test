1ce6cdb21510645ebde4bd113bc78bc8ed881463
Ana Seiculescu <anaseiculescu@gmail.com>
2024-08-13 12:38:53 +0300

    refactoring the database setup, completing the ArtworksManager component with all CRUD operations

--- backend/database/dbsetup.js
+++ backend/database/dbsetup.js
@@ -14,40 +14,99 @@ const dbClient = knex({
 });
 
 async function run() {
-    await dbClient.raw(
-        `
-        drop table if exists artistwork;
-        
-        create table artistwork (
-            id serial primary key,
-            title varchar not null,
-            description varchar,
-            image varchar,
-            clientlink varchar,
-            is_visible boolean not null
-        );
+    await dbClient.schema.dropTableIfExists("artistwork");
 
-        insert into artistwork (
-            title, description, image, clientlink, is_visible
-        ) values (
-            'among trees', 'nature and mystery under the colorful light, playing and dancing for the Earth', '/images/among-trees.jpg', 'https://www.google.com', true
-        ),
-        (
-            'shot', 'finding yourself after...', '/images/shot.jpg', 'https://www.google.com', true
-        ),
-        (
-            'minimalistic-desktop', 'what if today is not just today, what if today would be everywhere, anytime, what if all is just today...what if...', '/images/minimalistic-desktop.jpg', 'https://www.google.com', true
-        ),
-        (
-            'tree', 'quiet childhood, but not alone', '/images/tree.jpg', 'https://www.google.com', true
-        ),
-        (
-            'mountain-landscape', 'the gradient of nature in reverse', '/images/mountain-landscape.jpg', 'https://www.google.com', true
-        ),
-        (
-            'minimal', 'beautiful danger, silent nature, teamwork...will you come and visit', '/images/minimal.jpg', 'https://www.google.com', true
-        );`
-    );
+    await dbClient.schema.createTable("artistwork", (table) => {
+        table.increments("id").primary();
+        table.string("title").notNullable();
+        table.string("description");
+        table.string("image");
+        table.string("clientlink");
+        table.boolean("is_visible").notNullable();
+    });
+
+    await dbClient("artistwork").insert([
+        {
+            title: "among trees",
+            description: "nature and mystery under the colorful light, playing and dancing for the Earth",
+            image: "/images/among-trees.jpg",
+            clientlink: "https://www.deviantart.com/freya-passifolle/art/Among-Trees-Small-Group-884376795",
+            is_visible: true,
+        },
+        {
+            title: "shot",
+            description: "finding yourself after...",
+            image: "/images/shot.jpg",
+            clientlink: "https://www.deviantart.com/josemau1900/art/Shot-18-400032201",
+            is_visible: true,
+        },
+        {
+            title: "minimalistic-desktop",
+            description:
+                "what if today is not just today, what if today would be everywhere, anytime, what if all is just today...what if...?",
+            image: "/images/minimalistic-desktop.jpg",
+            clientlink: "https://www.deviantart.com/jmnaleef/art/Minimalistic-Desktop-903011560",
+            is_visible: true,
+        },
+        {
+            title: "somewhere",
+            description: "quiet childhood, but not alone",
+            image: "/images/somewhere.jpg",
+            clientlink: "https://www.deviantart.com/djmattricks/art/Somewhere-52892874",
+            is_visible: true,
+        },
+        {
+            title: "mountain-landscape",
+            description: "the gradient of nature in reverse",
+            image: "/images/mountain-landscape.jpg",
+            clientlink: "https://www.deviantart.com/rahasqc/art/Flat-Landscape-Wallpaper-1-668185454",
+            is_visible: true,
+        },
+        {
+            title: "minimal",
+            description: "beautiful danger, silent nature, teamwork...will you come and visit?",
+            image: "/images/minimal.jpg",
+            clientlink: "https://www.deviantart.com/docberlin77/art/Minimal-Part-II-446426920",
+            is_visible: true,
+        },
+    ]);
 }
+
+// async function run() {
+//     await dbClient.raw(
+//         `
+//         drop table if exists artistwork;
+
+//         create table artistwork (
+//             id serial primary key,
+//             title varchar not null,
+//             description varchar,
+//             image varchar,
+//             clientlink varchar,
+//             is_visible boolean not null
+//         );
+
+//         insert into artistwork (
+//             title, description, image, clientlink, is_visible
+//         ) values (
+//             'among trees', 'nature and mystery under the colorful light, playing and dancing for the Earth', '/images/among-trees.jpg', 'https://www.google.com', true
+//         ),
+//         (
+//             'shot', 'finding yourself after...', '/images/shot.jpg', 'https://www.google.com', true
+//         ),
+//         (
+//             'minimalistic-desktop', 'what if today is not just today, what if today would be everywhere, anytime, what if all is just today...what if...\\?', '/images/minimalistic-desktop.jpg', 'https://www.google.com', true
+//         ),
+//         (
+//             'somewhere', 'quiet childhood, but not alone', '/images/somewhere.jpg', 'https://www.google.com', true
+//         ),
+//         (
+//             'mountain-landscape', 'the gradient of nature in reverse', '/images/mountain-landscape.jpg', 'https://www.google.com', true
+//         ),
+//         (
+//             'minimal', 'beautiful danger, silent nature, teamwork...will you come and visit\\?', '/images/minimal.jpg', 'https://www.google.com', true
+//         );`
+//     );
+// }
 
 run();
--- frontend/src/components/ArtworksManager.jsx
+++ frontend/src/components/ArtworksManager.jsx
@@ -9,15 +9,17 @@ export function ArtworksManager() {
     const [artworks, setArtworks] = useState([]);
     // const [loading, setLoading] = useState(true);
     // const [error, setError] = useState(null);
-    // const [newArtwork, setNewArtwork] = useState({
-    //     title: "",
-    //     description: "",
-    //     image: "",
-    //     clientlink: "",
-    //     isVisible: true,
-    // });
+    const [newArtwork, setNewArtwork] = useState({
+        title: "",
+        description: "",
+        image: "",
+        clientlink: "",
+        isVisible: true,
+    });
 
     const readAllURL = "http://localhost:3000/artworks/all";
+    const update_delete_URL = "http://localhost:3000/artworks/${id}";
+    const createURL = "http://localhost:3000/artworks";
 
     useEffect(() => {
         fetch(readAllURL)
@@ -28,11 +30,60 @@ export function ArtworksManager() {
                 // setLoading(false);
             })
             .catch((error) => {
-                console.log(error);
+                console.log("error: ", error);
                 // setError(error);
                 // setLoading(false);
             });
     }, []);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewArtwork({
+            ...newArtwork,
+            [name]: value,
+        });
+    };
+
+    const addArtwork = () => {
+        fetch(createURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newArtwork),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setArtworks([...artworks, data]);
+                setNewArtwork({ title: "", description: "", image: "", clientlink: "", isVisible: true });
+            })
+            .catch((error) => console.log("error: ", error));
+    };
+
+    const updateArtwork = (id, updatedArtwork) => {
+        fetch(update_delete_URL, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedArtwork),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setArtworks(artworks.map((artwork) => (artwork.id === id ? data : artwork)));
+            })
+            .catch((error) => console.log("error: ", error));
+    };
+
+    const deleteArtwork = (id) => {
+        fetch(update_delete_URL, {
+            method: "DELETE",
+        })
+            .then(() => {
+                setArtworks(artworks.filter((artwork) => artwork.id !== id));
+            })
+            .catch((error) => console.log("error: ", error));
+    };
 
     const artworksStyle = {
         width: "100%",
@@ -48,7 +99,7 @@ export function ArtworksManager() {
                 <HeaderMenu />
                 <ResponsiveMasonry
                     // columnsCountBreakPoints={{ 480: 1, 768: 2, 1100: 3, 1380: 4, 1690: 5 }}
-                    columnsCountBreakPoints={{ 380: 1, 550: 2, 930: 3, 1250: 4, 1650: 5 }}
+                    columnsCountBreakPoints={{ 380: 1, 550: 2, 930: 3, 1350: 4, 1750: 5 }}
                     style={{ margin: "25px 25px 25px 30px" }}
                 >
                     <Masonry gutter="20px">
